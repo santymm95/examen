c165fda3a5f1775c7f7fc1747401031725224eef
El parche del pablo <El parche del pablo@DESKTOP-1SEHLPP>
2024-03-08 20:31:25 -0500

    paso 1 definir la comision y el sueldo base, creamos todas las sumas donde van representadas las 3 ventas.

---
+++ examenUno/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace examenUno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sueldoBase = 1200000;
+            double comisionPorVenta = 0.10;
+
+            // Definir las ventas del vendedor
+            double[] ventas = new double[] { 500000, 400000, 300000 }; // Ejemplo donde las ventas suman más de 1.000.000
+
+        }
+    }
+}
